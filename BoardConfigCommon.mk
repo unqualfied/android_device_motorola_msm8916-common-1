adf2914d7b485d2987bfdacbe0d33f7e36f8b1ff
Kyle Harrison <khwebmail@gmail.com>
2018-04-26 17:17:17 +0100

    LOS => DU

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,7 +126,7 @@ BOARD_PROVIDES_LIBRIL := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+#include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Shims
