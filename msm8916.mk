98f2bd5482a83364851474e253690bd37cf7172f
Kyle Harrison <khwebmail@gmail.com>
2018-05-23 11:46:33 +0100

    msm8916-common: Enable Gralloc1

--- msm8916.mk
+++ msm8916.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.hardware.renderscript@1.0-service \
+    libgenlock \
     gralloc.msm8916 \
     copybit.msm8916 \
     hwcomposer.msm8916 \
