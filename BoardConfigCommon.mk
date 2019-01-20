4e7897f37daf6662feb55fca4b18c598b2017ab0
Nico <nicorg2529@gmail.com>
2019-01-20 10:05:08 +0000

    msm8916-common: Merge RIL and QSAP shims

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,7 +157,6 @@ SELINUX_IGNORE_NEVERALLOWS := true
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/lib/libandroid.so|libshim_ril.so \
     /system/vendor/lib/libmdmcutback.so|libqsap_shim.so \
     /system/lib/libmot_sensorlistener.so|libsensor.so \
     /system/lib/libjustshoot.so|libshims_camera.so \
